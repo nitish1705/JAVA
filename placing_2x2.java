bd9a276356d29395ed323592ce18be25299a2848
Nitish M <mnitish1705@gmail.com>
2025-12-13 19:19:00 +0530

    removed

--- placing_2x2.java
+++ placing_2x2.java
@@ -1,7 +1,5 @@
 import java.util.*;
-
 public class placing_2x2 {
-
     static int[][] diff = {{-1, -1}, {-1, 0}, {-1, 1},{ 0, -1}, { 0, 0}, { 0, 1},{ 1, -1}, { 1, 0}, { 1, 1}};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
