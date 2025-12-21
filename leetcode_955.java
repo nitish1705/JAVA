ee9b1768ea57f77e250cd0c1e38df1ce6df89dab
Nitish M <mnitish1705@gmail.com>
2025-12-21 21:31:08 +0530

    leetcode

---
+++ leetcode_955.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class leetcode_955{
+	public static int minDeletionSize(String[] strs) {
+        int count = 0;
+		int r = strs.length;
+		int c = strs[0].length();
+		String[] arr = new String[r];
+		Arrays.fill(arr, "");
+		for(int i = 0; i < c; i++){
+			int flag = 0;
+			for(int j = 1; j < r; j++){
+				String s1 = arr[j-1] + strs[j-1].charAt(i);
+				String s2 = arr[j] + strs[j].charAt(i);
+				if(s1.compareTo(s2) > 0){
+					flag = 1;
+					count++;
+					break;
+				}
+			}
+			if(flag == 0){
+				for(int k = 0; k < r; k++){
+					arr[k] = arr[k] + strs[k].charAt(i);
+				}
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+		return count;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int res = minDeletionSize(new String[] {"xga", "xfb", "yfa"});
+		System.out.println(res);
+        sc.close();
+    }
+}
